08f7f369282353a5820fcada1d9bcfb25bc73320
StSour <zhora.skiba@mail.ru>
2026-05-10 10:02:27 +0300

    0.9v

---
+++ build/CMakeFiles/media_scanner.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/media_scanner.dir/link.d"
+  "CMakeFiles/media_scanner.dir/src/main.cpp.o"
+  "CMakeFiles/media_scanner.dir/src/main.cpp.o.d"
+  "media_scanner"
+  "media_scanner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/media_scanner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
